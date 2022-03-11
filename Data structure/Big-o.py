9c6048ab89d150838cfab32ad87b05459696b270
Devam <devamchangani@gmail.com>
2022-03-11 19:19:38 +0530

    commit

--- Data structure/Big-o.py
+++ Data structure/Big-o.py
@@ -1,3 +1,21 @@
-print("hello")
-print(709+79)
-print(121212111)
+import numbers
+import string
+
+
+# def get_squared_numbers(numbers):
+#     squared_numbers = []
+#     for n in numbers:
+#         squared_numbers.append(n*n)
+#     print(squared_numbers)
+
+# numbers = [2,5,6,9]
+# get_squared_numbers(numbers)
+
+numbers = [3,6,3,5,2,4,5]
+
+for i in range(len(numbers)):
+    for j in range(i+1, len(numbers)):
+        if numbers[i] == numbers[j]:
+            print(numbers[i] )
+            print("is a duplicate")
+            break
