4ef6389142c1803a62f36d84bd09da4e35912f6a
Devam changani <85890675+Devamchangani@users.noreply.github.com>
2022-03-15 19:06:27 +0530

    commit

--- Python - Basic/chapter - 2/01_variable.py
+++ Python - Basic/chapter - 2/01_variable.py
@@ -1,13 +1,25 @@
-a = "devam"        #string
-b = 123            #number
-c = 43.43          #floting number
+
+
+a = "devam"        #String
+b = 123            #Number
+c = 43.43          #Floting number
+d = True           # Booleans
+c = None           # None
+
+# -------------- printing the variable ------------
 
 print(a)
 print(b)
 print(c)
-print(496+652)
+print(d)
+print(c)
 
 
+# --------------- printing type of variable
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(c))
 
 
-48
