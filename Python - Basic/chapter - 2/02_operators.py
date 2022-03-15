98814291e35b27caf3f0b67e66c2c440363d0b6f
Devam changani <85890675+Devamchangani@users.noreply.github.com>
2022-03-15 19:42:15 +0530

    commit

--- Python - Basic/chapter - 2/02_operators.py
+++ Python - Basic/chapter - 2/02_operators.py
@@ -1,3 +1,6 @@
+from sqlalchemy import true
+
+
 a = 4
 b = 437
 
@@ -7,3 +10,34 @@ print("value of a+b", + a+b )
 print("value of a-b", + a-b )
 print("value of a*b", + a*b )
 print("value of a/b", + a/b )
+
+
+# ------------- Assignment Operators --------------
+
+a = 36
+a += 12
+a -= 12
+a *= 12
+a /= 12
+
+print(a)
+
+# --------------- Comparison operator --------------
+
+b = (23 > 45)
+b = (23 >= 45)
+b = (23 < 45)
+b = (23 <= 45)
+b = (23 == 45)
+b = (23 != 45)
+print(b)
+
+
+# --------------- Logical operator -------------
+
+bool1 = True
+bool2 = False
+
+print("value of bool1 and bool2", + (bool1 and bool2))
+print("value of bool1 or bool2", + (bool1 or bool2))
+print("value of bool1 not bool2", + ( not bool2))
