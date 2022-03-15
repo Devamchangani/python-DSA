76ff420b2082ba857728b0890621b36f18e3ef3e
Devam changani <85890675+Devamchangani@users.noreply.github.com>
2022-03-15 19:17:29 +0530

    commit

---
+++ Python - Basic/chapter - 2/02_operators.py
@@ -0,0 +1,9 @@
+a = 4
+b = 437
+
+# ------------- Arithmetic Operators -----------
+
+print("value of a+b", + a+b )
+print("value of a-b", + a-b )
+print("value of a*b", + a*b )
+print("value of a/b", + a/b )
