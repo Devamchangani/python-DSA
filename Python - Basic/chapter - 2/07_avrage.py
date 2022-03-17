4f373aa991f7752b0f46c8c34a1ea833887bc6e2
Devam changani <85890675+Devamchangani@users.noreply.github.com>
2022-03-17 19:32:53 +0530

    commit

---
+++ Python - Basic/chapter - 2/07_avrage.py
@@ -0,0 +1,12 @@
+a = int(input("Enter a first number :"))
+b =  int(input("Enter a second number :"))
+
+
+#------------ type casting string to int ----------
+
+# a = int(a)
+# b = int(b)
+
+avg = (a+b)/2
+
+print("The value of avrage of a and b", avg)
