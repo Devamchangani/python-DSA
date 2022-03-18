757255579ecff81376d75a00f2957866cbc561b1
Devam changani <85890675+Devamchangani@users.noreply.github.com>
2022-03-18 19:23:45 +0530

    commit

---
+++ Python - Basic/Basic projects/Chapter - 3/02_string_slicing.py
@@ -0,0 +1,13 @@
+greeting = "good morning   "
+
+name = "Devam"
+
+# # print(type(name))
+
+# concatinating two string
+print(greeting   + name)
+
+# print(name[4])
+# name[3] = "n" --> does not work
+
+print(name[1:3])
