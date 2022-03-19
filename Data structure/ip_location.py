e15a3c207e84ca8f75fac2849ab4f140764cbbd8
Devam changani <85890675+Devamchangani@users.noreply.github.com>
2022-03-19 12:36:27 +0530

    commit

--- Data structure/ip_location.py
+++ Data structure/ip_location.py
@@ -2,29 +2,29 @@
 import requests
 import json
 
-# #----------   single ip request   ------------
+#----------   single ip request   ------------
 
 
-# response = requests.get("http://ip-api.com/json/24.48.0.1").json()
-# print(response)
+response = requests.get("http://ip-api.com/json/24.48.0.1").json()
+print(response)
 
-# print(response['country'])
-
-
-
-# #----------   batch ip  request   -------------
+print(response['country'])
 
 
-# response = requests.post("http://ip-api.com/batch", json=[
-#     {"query":"208.80.152.201"},
-#     {"query":"168.71.3.52"},
-#     {"query":"206.189.198.234"},
-#     {"query":"157.230.75.212"}
-# ]).json()
 
-# for ip_info in response:
-#         print(ip_info["country"])
-#         print(ip_info["country"])
+#----------   batch ip  request   -------------
+
+
+response = requests.post("http://ip-api.com/batch", json=[
+    {"query":"208.80.152.201"},
+    {"query":"168.71.3.52"},
+    {"query":"206.189.198.234"},
+    {"query":"157.230.75.212"}
+]).json()
+
+for ip_info in response:
+        print(ip_info["country"])
+        print(ip_info["country"])
 
 
 
