a6f2bdb9b09776a944e0efc57d08a91729f3759f
Devam changani <85890675+Devamchangani@users.noreply.github.com>
2022-03-20 18:52:08 +0530

    commit

---
+++ Python - Basic/Chapter - 3/03 - string_function.py
@@ -0,0 +1,22 @@
+from re import T
+
+
+story = "This is a python course , this topic is a string function"
+
+# string functions
+
+# len function
+print(len(story))
+print(story.endswith("function"))
+
+# count function
+print(story.count("p")) 
+
+# capitalige function
+print(story.capitalize())
+
+# find function
+print(story.find("python"))
+
+# replase function
+print(story.replace("is","as"))
