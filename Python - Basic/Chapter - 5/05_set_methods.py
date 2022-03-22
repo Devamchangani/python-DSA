38c591f123d7da1be0a6444531faa82f93278f27
Devam changani <85890675+Devamchangani@users.noreply.github.com>
2022-03-22 19:49:57 +0530

    commit

--- Python - Basic/Chapter - 5/05_set_methods.py
+++ Python - Basic/Chapter - 5/05_set_methods.py
@@ -1,14 +1,25 @@
 # creating an empty set
 
+from os import remove
+
+
 b = set()
 print(type(b))
 
 # adding a value in set
 b.add(8)
 b.add(17)
-
+b.add(17)# adding a value repeatedly does not changes a set
 
 b.add((5, 8, 9)) #can add tuple to set
+
 #b.add({4:5}) #cannot add list or dictionory to set
+
+
+# print a lrnghth of set
+print(len(b))
+
+# remove the value
+b.remove(17)
 
 print(b)
