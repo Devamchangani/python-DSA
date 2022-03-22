8f7ab603d18edc15f78def26fb46c2221f67e958
Devam changani <85890675+Devamchangani@users.noreply.github.com>
2022-03-22 19:07:47 +0530

    commit

---
+++ Python - Basic/Chapter - 5/01_dictionary_syntez.py
@@ -0,0 +1,12 @@
+MyDict = {
+    "car": "this is a fast",
+    "Devam": "A coder",
+    "list": [52, 85, 76, 25],
+    "anotherdict": {'Devam': 'player'}
+
+}
+
+print(MyDict["car"])
+print(MyDict["Devam"])
+print(MyDict["list"])
+print(MyDict['anotherdict']['Devam'])
