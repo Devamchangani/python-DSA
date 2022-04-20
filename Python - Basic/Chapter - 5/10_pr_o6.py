4ff61e6336f843251b3ad8ff90fa507c687a733c
Devam changani <85890675+Devamchangani@users.noreply.github.com>
2022-04-20 18:57:02 +0530

    commit

---
+++ Python - Basic/Chapter - 5/10_pr_o6.py
@@ -0,0 +1,14 @@
+favlang = {}
+a = input("Enter your favorite language sujal \n")
+b = input("Enter your favorite language jay \n")
+c = input("Enter your favorite language sahil \n")
+d = input("Enter your favorite language meet \n")
+e = input("Enter your favorite language yash \n")
+
+favlang['sujal'] = a 
+favlang['jay'] = b 
+favlang['sahil'] = c 
+favlang['meet'] = d 
+favlang['yash'] = e
+
+print(favlang)
