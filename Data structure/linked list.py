a423112fdcc3c07ec988c667e42e5a3a07cf32a2
Devam changani <85890675+Devamchangani@users.noreply.github.com>
2022-06-22 19:25:51 +0530

    commit

--- Data structure/linked list.py
+++ Data structure/linked list.py
@@ -1,6 +1,7 @@
 from dataclasses import dataclass
 from itertools import count
 from platform import node
+from secrets import choice
 
 
 class Node:
@@ -46,7 +47,7 @@ class Linkedlist:
             itr = itr.next
             count += 1
 
-    def remove_at(self, index, data):
+    def remove_at(self, index):
         if index < 0 or index > self.get_length():
             raise Exception("Invalid index")
 
@@ -64,6 +65,11 @@ class Linkedlist:
             itr = itr.next
             count += 1
         
+
+    def insert_values(self, data_list):
+        self.head = None
+        for data in data_list:
+            self.insert_at_end(data)
 
     def print(self):
         itr = self.head
@@ -88,13 +94,68 @@ class Linkedlist:
 
 if __name__ == '__main__':
     root = Linkedlist()
-    root.insert_at_begining(5)
-    root.insert_at_begining(10)
-    root.insert_at_begining(15)
-    root.insert_at_end(254)
-    root.insert_at_middel(3, 263)
-    root.insert_at_middel(1, 999)
-    root.print()
-    root.remove_at(3)
-    root.print()
-    print(root.get_length())
+    # ===============================first-insert * last-insert * middle-insert * remove =====================================
+
+    # root.insert_at_begining(5)
+    # root.insert_at_begining(10)
+    # root.insert_at_begining(15)
+    # root.insert_at_end(254)
+    # root.insert_at_middel(3, 263)
+    # root.insert_at_middel(1, 999)
+    # root.print()
+    # root.remove_at(3)
+    # root.remove_at(1)
+    # root.print()
+    # print(root.get_length())
+
+# =================================== Insert a list ==========================================
+
+    # root.insert_values(["banana", "grapes", "mango", "orange", "apple"])
+    # root.insert_at_middel(1, "blueberry")
+    # root.print()
+
+
+    while True:
+        print("************************************* SINGLY LINKED LIST *************************************")
+        print("1. Insert value at begining")
+        print("2. Insert value at end ")
+        print("3. Insert Value at middle")
+        print("4. Remove value")
+        print("5. length of list")
+        print("6. Exit")
+        choice = int(input("Enter Your choise: "))
+        if choice == 1:
+                num = 1
+                a = int(input("enter your lenght of list : "))
+                
+                while(num <= a):
+                    b = int(input(f"Enter your {num} value : "))
+                    root.insert_at_begining(b)
+                    num += 1
+                root.print()
+
+        elif choice == 2:
+                num = 1
+                a = int(input("enter your lenght of list : "))
+                
+                while(num <= a):
+                    b = int(input(f"Enter your {num} value : "))
+                    root.insert_at_end(b)
+                    num += 1
+                root.print()
+          
+        elif choice == 3:
+                a = int(input("Enter a position : "))
+                b = int(input("Enter a value : "))
+                root.insert_at_middel(a, b)
+                root.print()
+        elif choice == 4:
+                a = int(input(" Enter your position :"))
+                # b = int(input("Enter your delete value : "))
+                root.remove_at(a)
+                root.print()
+        elif choice == 5:
+                print(root.get_length())
+        elif choice == 6:
+                print("")
+                break
