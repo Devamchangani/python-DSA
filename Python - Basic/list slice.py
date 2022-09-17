9065ad6b4e00c93f35338dc6987f323822d9c5f2
Devam changani <85890675+Devamchangani@users.noreply.github.com>
2022-09-18 04:21:21 +0530

    commit

--- Python - Basic/list slice.py
+++ Python - Basic/list slice.py
@@ -1,9 +1,27 @@
-# Initialize list
-Lst = [50, 70, 30, 20, 90, 10, 50]
+# # Initialize list
+# Lst = [-2,]
  
-# Display list
-print(Lst[::])
-print(Lst[1::])
-print(Lst[1:5:])
-print(Lst[1:5:2])
-print(Lst[::-1])
+# # Display list
+# print(Lst[::])
+# print(Lst[1::])
+# print(Lst[1:5:])
+# print(Lst[1:5:2])
+# print(Lst[::-1])
+
+# Initializing list
+test_list = [1, 2, 3, 1, 3, 4]
+ls = []
+ls1 = []
+# Checking if 4 exists in list
+for i in test_list:
+    if(i == 1 or i == 2):
+        ls.append(i+3)
+print(ls)      
+
+
+ls1.append(test_list[4]-7)
+
+print(ls1)
+
+Sum = sum(test_list[3:7:])
+print(Sum)
