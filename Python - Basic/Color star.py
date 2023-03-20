a3075b28eb55d51719ef7685c9f22fa333ebd886
Devam changani <85890675+Devamchangani@users.noreply.github.com>
2023-03-21 00:13:10 +0530

    commit

--- Python - Basic/Color star.py
+++ Python - Basic/Color star.py
@@ -1,11 +1,13 @@
 import turtle
-turtle.pensize(3)
+turtle.pensize(6)
 turtle.speed(200)
 turtle.bgcolor("black")
+turtle.left(70)
+
 
 for i in range(8):
  for colours in ["yellow","blue","white","green","red","pink","purple"]:
   turtle.color(colours)
   turtle.left(100)
-  turtle.left(60)
+  turtle.left(120)
   turtle.forward(500)
