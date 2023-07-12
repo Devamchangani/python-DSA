31c31b8e5b40ab073628d8acd13c69a1c11af8a4
Devam changani <85890675+Devamchangani@users.noreply.github.com>
2023-07-12 18:20:24 +0530

    commit

---
+++ GFG/Power of number.py
@@ -0,0 +1,42 @@
+#User function Template for python3
+
+class Solution:
+    #Complete this function
+    def power(self,N,R):
+        result = 1
+        base = N
+        exponent = R
+        modulus = 10**9 + 7
+
+        while exponent > 0:
+            if exponent % 2 == 1:
+                result = (result * base) % modulus
+            base = (base * base) % modulus
+            exponent //= 2
+
+        return result
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+def main():
+    
+    T=int(input())
+    
+    while(T>0):
+        
+        N=input()
+        R=N[::-1]
+        
+        ob=Solution();
+        ans=ob.power(int(N),int(R))
+        print(ans)
+        
+        T-=1
+
+1if __name__=="__main__":
+    main()
+# } Driver Code Ends
