b514704785df8e50ee7e6ef8bd32e683ae3a4ede
Devam changani <85890675+Devamchangani@users.noreply.github.com>
2023-07-22 18:25:12 +0530

    commit

---
+++ GFG/SCHOOL/reverse a list in group of size.py
@@ -0,0 +1,27 @@
+def rev(data,k):
+    # dict = {}
+    l = len(data)
+    a = []
+    print("")
+    for i in range(0,l,k):
+        for j in range(i,i+k):
+            # print(data[j])
+            # b = data[j]
+            # a.append(data[j])
+            if j < l:
+                a.insert(i, data[j])
+            else:
+                break
+
+        # print("")
+    print(a)
+
+
+
+if  __name__ == "__main__":
+    tc = int(input())
+    while tc > 0:
+        data = [str(x)for x in input("Enter the data: ").split()]
+        k = int(input())
+        rev(data,k)
+        tc -= 1
