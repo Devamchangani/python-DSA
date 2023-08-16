fcf09947f13c91472e02965982182ecaf911b2c3
Devam changani <85890675+Devamchangani@users.noreply.github.com>
2023-08-16 12:51:09 +0530

    commit

--- GFG/Problem of the day/Nth catalan number.py
+++ GFG/Problem of the day/Nth catalan number.py
@@ -16,19 +16,19 @@ class Solution:
         # return res%m
 
 #   LESS TIME TAKEN COMPARE TO RECURSION (MEDIAN CASE)
-        if (n == 0 or n == 1):
-            return 1
-        # divide table
-        catalan = [0 for i in range(n + 1)]
-        # Initialization
-        catalan[0] = 1
-        catalan[1] = 1
-        # recursion
-        for i in range(2, n + 1):
-           catalan[i] = 0
-           for j in range(i):
-              catalan[i] = catalan[i] + catalan[j] * catalan[i-j-1]
-        return catalan[n]%m
+        # if (n == 0 or n == 1):
+        #     return 1
+        # # divide table
+        # catalan = [0 for i in range(n + 1)]
+        # # Initialization
+        # catalan[0] = 1
+        # catalan[1] = 1
+        # # recursion
+        # for i in range(2, n + 1):
+        #    catalan[i] = 0
+        #    for j in range(i):
+        #       catalan[i] = catalan[i] + catalan[j] * catalan[i-j-1]
+        # return catalan[n]%m
         
 
 
