e7521496e4a985dc5b366e404fd1f3ac323c33cb
Devam changani <85890675+Devamchangani@users.noreply.github.com>
2023-08-20 14:49:05 +0530

    COMMIT

---
+++ GFG/Problem of the day/sum of given subarray.py
@@ -0,0 +1,49 @@
+#User function Template for python3
+
+#Function to find a continuous sub-array which adds up to a given number.
+class Solution:
+    def subArraySum(self,arr, n, s): 
+       #Write your code here
+        sum = 0
+        i = 0
+        j = 0
+        while(j<n):
+            sum += arr[j]
+            while(i<j and sum > s):
+                sum -= arr[i]
+                i += 1
+            if ( sum == s):
+                return [i+1, j+1]
+            j += 1
+        return [-1]
+
+
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+def main():
+        T=int(input())
+        while(T>0):
+            
+            NS=input().strip().split()
+            N=int(NS[0])
+            S=int(NS[1])
+            
+            A=list(map(int,input().split()))
+            ob=Solution()
+            ans=ob.subArraySum(A, N, S)
+            
+            for i in ans:
+                print(i, end=" ")
+                
+            print()
+            
+            T-=1
+
+
+if __name__ == "__main__":
+    main()
+# } Driver Code Ends
