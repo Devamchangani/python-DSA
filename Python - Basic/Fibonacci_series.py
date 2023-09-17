3a61cc69058fd343e56af6f1543000d923039478
Devam changani <85890675+Devamchangani@users.noreply.github.com>
2023-09-17 11:34:07 +0530

    commit

--- Python - Basic/Fibonacci_series.py
+++ Python - Basic/Fibonacci_series.py
@@ -1,16 +1,20 @@
 def fibonacci():
     n = int(input("Enter a number: "))
     print(f"Fibonacci series upto {n} is: ")
-    f = 1
-    s = 1
-    print(f)
-    print(s)
+    a = 1
+    b = 1
+    l = [1]
+    if n == 1:
+        return l
+    l.append(b)
     for i in range(n-2):
-        l = f+s
-        print(l)
-        s = f
-        f = l
-    print(f)
+        c = a+b
+        a = b
+        b = c
+        l.append(c)
+    return l
 
 if __name__ == "__main__":
-    fibonacci()
+    res = fibonacci()
+    for i in res:
+        print(i, end=" ")
