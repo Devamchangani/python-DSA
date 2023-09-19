b5c6c32205028f19a886c5bd2108b11314674806
Devam changani <85890675+Devamchangani@users.noreply.github.com>
2023-09-19 13:16:54 +0530

    commit

---
+++ GFG/Problem of the day/Find first set bit.py
@@ -0,0 +1,53 @@
+#{ 
+ # Driver Code Starts
+#Initial Template for Python 3
+
+import math
+
+
+
+# } Driver Code Ends
+#User function Template for python3
+
+class Solution:
+    
+    #Function to find position of first set bit in the given number.
+    def getFirstSetBit(self,n):
+        #Your code here
+        if n == 0:
+            return 0
+        c = 1
+        while(n):
+            if n & 1:
+                return c
+            n >>= 1
+            c += 1
+        return c
+
+#{ 
+ # Driver Code Starts.
+    
+    
+def main():
+    
+    T=int(input())
+    
+    while(T>0):
+        
+        
+        n=int(input())
+        ob=Solution()
+        
+        print(ob.getFirstSetBit(n))
+        
+        
+        T-=1
+    
+    
+
+
+
+
+if __name__=="__main__":
+    main()
+# } Driver Code Ends
